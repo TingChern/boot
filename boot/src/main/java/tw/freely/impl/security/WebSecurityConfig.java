0920a5ea7a9756deea889425d2a84a18990e5c2c
ting.chen <ny88619@gmail.com>
2021-06-17 19:54:17 +0800

    temp remove security

--- boot/src/main/java/tw/freely/impl/security/WebSecurityConfig.java
+++ boot/src/main/java/tw/freely/impl/security/WebSecurityConfig.java
@@ -4,11 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
+//@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
-	}
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+//	}
 }
