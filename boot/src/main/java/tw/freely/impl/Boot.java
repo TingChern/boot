95e4b76ec33dd565a8b399e15fe067dc4f1fa799
ting.chen <ny88619@gmail.com>
2021-06-22 16:55:44 +0800

    test line bot api

--- boot/src/main/java/tw/freely/impl/Boot.java
+++ boot/src/main/java/tw/freely/impl/Boot.java
@@ -3,6 +3,11 @@ package tw.freely.impl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
 @SpringBootApplication
@@ -12,5 +17,15 @@ public class Boot{
 	public static void main(String[] args) {
 		SpringApplication.run(Boot.class, args);
 	}
+	
+	@EventMapping
+	public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+		System.out.println("event: " + event);
+		return new TextMessage(event.getMessage().getText());
+	}
 
+	@EventMapping
+	public void handleDefaultMessageEvent(Event event) {
+		System.out.println("event: " + event);
+	}
 }
